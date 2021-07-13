606c97dd555a44ab84ead18bf95bd118ea79bb90
Yusei Okada <yusei42108991@gmail.com>
2021-07-13 18:29:46 +0900

    fix Eslint error.

--- variables/layout.js
+++ variables/layout.js
@@ -1,6 +1,7 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useMediaQuery } from "@material-ui/core/useMediaQuery"
 
 export const isMobileOrTablet = () => {
-    const isMobileOrTablet = !!useMediaQuery(theme => theme.breakpoints.down('sm'));
+    const isMobileOrTablet = useMediaQuery(theme => theme.breakpoints.down('sm'));
     return { isMobileOrTablet };
 }
