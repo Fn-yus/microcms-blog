4b7f5dcfcc45b976cfa53064526aa3d575b73725
Yusei Okada <yusei42108991@gmail.com>
2021-11-07 12:01:22 +0900

    fix FOUC.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -18,6 +18,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script>0</script>
         </body>
       </Html>
     );
