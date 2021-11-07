34bf40ec6295994bb389ebab4b141c2a815b8825
Yusei Okada <yusei42108991@gmail.com>
2021-11-07 18:55:41 +0900

    add jssprovider for fouc.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,24 +2,27 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../commons/theme';
+import { JssProvider } from 'react-jss';
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head>
-          {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
+      <JssProvider>
+        <Html lang="en">
+          <Head>
+            {/* PWA primary color */}
+            <meta name="theme-color" content={theme.palette.primary.main} />
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            />
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      </JssProvider>
     );
   }
 }
