2e8fd99fa1dca25da3b7c798c267b5b703a40847
Yusei <yusei42108991@gmail.com>
2023-01-21 20:31:56 +0900

    fix css.

--- src/utils/layout.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { useMediaQuery } from "@material-ui/core/useMediaQuery"
-
-export const isMobileOrTablet = () => {
-    const isMobileOrTablet = useMediaQuery(theme => theme.breakpoints.down('sm'));
-    return { isMobileOrTablet };
-}
