75bbf2e00792d20779febd2dd03dee7e1697ebd3
Fn-yus <yusei42108991@gmail.com>
2024-01-21 11:18:58 +0900

    create backend.

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -14,6 +14,7 @@ import { sortAllBlogs } from '../../utils/blog.js';
 import { RichEditorField } from '../../components/RichEditorField';
 import { currentUrl } from "../../utils/url";
 import { RichEditorField } from '../../components/RichEditorField';
+import { currentUrl } from "../../utils/url";
 
 // import { Data } from '../../interfaces'
 
