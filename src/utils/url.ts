a99f13834d4054dd44cd497d83ea20bf8769e1e5
Fn-yus <yusei42108991@gmail.com>
2024-01-21 11:28:47 +0900

    Revert "change api url."
    This reverts commit 2f9d6a551f1e84eef0503ca5ec598a432bae0958.

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,3 @@
 // VERCEL_URLは自動割当される
 
-export const currentUrl = process.env.STATUS === "local" ? "http://localhost:3000" : `https://fn-yus.vercel.app/`
+export const currentUrl = process.env.STATUS === "local" ? "http://localhost:3000" : `https://${process.env.VERCEL_URL}`
